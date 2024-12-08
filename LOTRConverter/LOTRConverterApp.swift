0387459e3df76f3a8f41379a0b42d85ba8887f3b
Jatin Foujdar <jatin321001@gmail.com>
2024-12-08 13:16:23 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by jatin foujdar on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
