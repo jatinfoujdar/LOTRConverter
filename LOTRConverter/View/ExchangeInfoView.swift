49dbdfd576959eff477f9c3d8879a44bb33ceb37
Jatin Foujdar <jatin321001@gmail.com>
2024-12-09 20:36:49 +0530

    exchange file commit

---
+++ LOTRConverter/View/ExchangeInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfoView.swift
+//  LOTRConverter
+//
+//  Created by jatin foujdar on 09/12/24.
+//
+
+import SwiftUI
+
+struct ExchangeInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfoView()
+}
