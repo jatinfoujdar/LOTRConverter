9b212733a5e5ca3aa79b34048dd06de6cd02a4c3
Jatin Foujdar <jatin321001@gmail.com>
2024-12-11 17:38:20 +0530

    currency icon commit

---
+++ LOTRConverter/View/CurrencyIconView.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyIconView.swift
+//  LOTRConverter
+//
+//  Created by jatin foujdar on 11/12/24.
+//
+
+import SwiftUI
+
+struct CurrencyIconView: View {
+    let currencyImage : ImageResource
+    let currencyName: String
+    
+    var body: some View {
+        ZStack(alignment: .bottom){
+            Image(currencyImage)
+                .resizable()
+                .scaledToFit()
+            
+            Text(currencyName)
+                .padding(3)
+                .font(.caption)
+                .frame(maxWidth: .infinity)
+                .background(.brown.opacity(0.75))
+        }
+        .padding(3)
+        .frame(width: 100,height: 100)
+        .background(.brown)
+        .clipShape(.rect(cornerRadius: 25))
+    }
+}
+
+#Preview {
+    CurrencyIconView(currencyImage: .goldpiece, currencyName: "Gold Piece")
+}
---
+++ LOTRConverter/View/SelectCurrencyView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct SelectCurrencyView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack(alignment: .center){
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack{
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+               
+               
+                
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                
+                HStack{
+                    
+                }
+                HStack{
+                    
+                }
+                
+                Button("Done"){
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+        }
+    }
+}
+
+
+#Preview(){
+    SelectCurrencyView( )
+}
