9a6fef0460c862733b73e23a35501ab92df22d7d
Jatin Foujdar <jatin321001@gmail.com>
2025-01-03 14:14:42 +0530

    make icon grid tapable commit

--- LOTRConverter/View/SelectCurrencyView.swift
+++ LOTRConverter/View/SelectCurrencyView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct SelectCurrencyView: View {
 
     @Environment(\.dismiss) var dismiss
+    @State var currency : Currency
     
     var body: some View {
         ZStack(alignment: .center){
@@ -16,7 +17,21 @@ struct SelectCurrencyView: View {
                     .fontWeight(.bold)
                 LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
                     ForEach(Currency.allCases){ currency in
-                        CurrencyIconView(currencyImage: currency.image, currencyName: currency.name)
+                        if self.currency == currency{
+                            CurrencyIconView(currencyImage: currency.image, currencyName: currency.name)
+                                .shadow(color: .black, radius: 10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(lineWidth: 3)
+                                        .opacity(0.5)
+                                )
+                        }
+                        else{
+                            CurrencyIconView(currencyImage: currency.image, currencyName: currency.name)
+                                .onTapGesture {
+                                    self.currency = currency
+                                }
+                        }
                     }
                 }
                
@@ -48,5 +63,5 @@ struct SelectCurrencyView: View {
 
 
 #Preview(){
-    SelectCurrencyView( )
+    SelectCurrencyView(currency: .silverPenny)
 }
